8da88975bd5a807024c988f283ed1e2db9a9545c
amin <yaghmaeeamin@yahoo.com>
2022-10-13 12:37:47 +0100

    icons hover fixed

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,18 +5,18 @@ const Footer = () => {
     <div id="footer">
       <div className="blur footer-blur"></div>
       <ul>
-        <li className="icons">
+        <li>
           <a href="https://www.instagram.com/sorena_gym_club/">
-            <i className="fa fa-instagram"></i>
+            <i className="fa fa-instagram icons"></i>
           </a>
         </li>
-        <li className="icons">
+        <li>
           <a href="https://web.telegram.org/z/#361011462">
-            <i className="fa fa-telegram"></i>
+            <i className="fa fa-telegram icons"></i>
           </a>
         </li>
-        <li className="icons">
-          <i className="fa fa-whatsapp"></i>
+        <li>
+          <i className="fa fa-whatsapp icons"></i>
         </li>
       </ul>
     </div>
